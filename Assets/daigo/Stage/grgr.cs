8b055190f03d1baaa69a65312555eb0b1c39aa3a
s16007 <s16007@std.it-college.ac.jp>
2016-08-07 16:07:25 +0900

    ステージの追加

---
+++ Assets/daigo/Stage/grgr.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class grgr : MonoBehaviour {
+	public float rotateX = 0.0f,
+		rotateY = 1.0f,
+		rotateZ = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (rotateX, rotateY, rotateZ);
+	}
+}
